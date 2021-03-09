ec7431253ee786986d7db894ca2e5fd8432401ad
Vladislav <vsakayafignavlad@gmail.com>
2021-03-09 12:37:22 +0300

    joke for my best friend
    making commits

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          First react dev edit
+          Гардей пососи тунца
         </p>
       </header>
     </div>
